dbba0b390277120d16525bcce04aa6e8e3a65f4b
rojae <rojae@kakao.com>
2026-01-10 03:28:20 +0900

    leetcode: problem sovling

---
+++ 04. PS/Leetcode/207.course-schedule.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=207 lang=java
+ *
+ * [207] Course Schedule
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] graph = new ArrayList[numCourses];
+        int[] indegree = new int[numCourses];
+
+        for(int i=0; i<numCourses; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int[] pre : prerequisites){
+            int course = pre[0];
+            int preCourse = pre[1];
+
+            graph[preCourse].add(course);
+            indegree[course]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        int count = 0;
+        while(!queue.isEmpty()){
+            int course = queue.poll();
+            count++;
+
+            for(int next : graph[course]){
+                indegree[next]--;
+                if(indegree[next] == 0){
+                    queue.add(next);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+}
+// @lc code=end
+
